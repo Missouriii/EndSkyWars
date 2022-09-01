c0e1d4a29ddb58367e0cacc677162a7a54081f0f
Missouriii <105463867+Missouriii@users.noreply.github.com>
2022-09-01 19:27:40 -0400

    Update MegaMode.php

--- src/hachkingtohach1/SkyWars/entity/MegaMode.php
+++ src/hachkingtohach1/SkyWars/entity/MegaMode.php
@@ -65,7 +65,8 @@ class MegaMode extends Entity{
 		$countDouble = SkyWars::getInstance()->getCountDoubleArena("mega");
 		$total = $countNormal + $countDouble;
 		$this->setNameTag(
-		    TextFormat::BOLD.TextFormat::YELLOW."Skywars Mega §8[§bNormal§r/§gDouble§8]\n".
+		    TextFormat::BOLD.TextFormat::YELLOW."Skywars Mega\n".
+			TextFormat::RESET.TextFormat::RESET."§8[§bNormal§r/§gDouble§8]\n".
 			TextFormat::RESET.TextFormat::RESET."Click to play!\n".
 			TextFormat::BOLD.TextFormat::GREEN. number_format($total)." Players"
 		);
