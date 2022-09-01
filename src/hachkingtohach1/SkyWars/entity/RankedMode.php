692c716ce4388db9f10793e00104bf8f5d8c6c5b
Missouriii <105463867+Missouriii@users.noreply.github.com>
2022-09-01 19:15:04 -0400

    Update RankedMode.php

--- src/hachkingtohach1/SkyWars/entity/RankedMode.php
+++ src/hachkingtohach1/SkyWars/entity/RankedMode.php
@@ -65,8 +65,8 @@ class RankedMode extends Entity{
 		$countInsane = SkyWars::getInstance()->getTotalCountPlayers("insane");
 		$total = $countNormal + $countInsane;
 		$this->setNameTag(
-		    TextFormat::BOLD.TextFormat::YELLOW."CLICK TO PLAY\n".
-			TextFormat::RESET.TextFormat::GRAY."Ranked Mode [Solo/Doubles]\n".
+		    TextFormat::BOLD.TextFormat::YELLOW."Skywars Ranked §8[§bNormal§r/§gInsane§8]\n".
+			TextFormat::RESET.TextFormat::RESET."Click to play!\n".
 			TextFormat::BOLD.TextFormat::GREEN. number_format($total)." Players"
 		);
 		$this->setNameTagVisible(true);
