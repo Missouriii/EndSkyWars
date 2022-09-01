fbba03cfee8468ece43be527d2a02e2428fc3f05
Missouriii <105463867+Missouriii@users.noreply.github.com>
2022-09-01 19:41:22 -0400

    fixed?

--- src/hachkingtohach1/SkyWars/entity/SoloMode.php
+++ src/hachkingtohach1/SkyWars/entity/SoloMode.php
@@ -66,7 +66,7 @@ class SoloMode extends Entity{
 		$total = $countNormal + $countInsane;
 		$this->setNameTag(
 		    TextFormat::BOLD.TextFormat::YELLOW."Skywars Solo \n".
-			TextFormat::RESET.TextFormat::RESET."§8[§bNormal§r/§gInsane§8]\n"
+			TextFormat::RESET.TextFormat::RESET."§8[§bNormal§r/§gInsane§8]\n".
 			TextFormat::RESET.TextFormat::RESET."Click to play!\n".
 			TextFormat::BOLD.TextFormat::GREEN. number_format($total)." Players"
 		);
